27c48eb917a5c22cdba3d92be4d91bd6a6337f54
Azraelos <andrewsugiarto93@gmail.com>
2017-02-04 20:19:45 +0800

    Split Address into Classes

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,13 +8,21 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must be in this format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
+    public static final int INDEX_BLOCK = 0;
+    public static final int INDEX_STREET = 1;
+    public static final int INDEX_UNIT = 2;
+    public static final int INDEX_POSTALCODE = 3;
+    
+    public final Block block;
+    public final Street street;
+    public final Unit unit;
+    public final PostalCode postalCode;
     private boolean isPrivate;
-
+    
     /**
      * Validates given address.
      *
@@ -22,11 +30,19 @@ public class Address {
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
-        if (!isValidAddress(trimmedAddress)) {
-            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        String[] addressComponents = trimmedAddress.split(",");
+        for(int i=0; i<addressComponents.length; i++){
+           	addressComponents[i] = addressComponents[i].trim();
         }
-        this.value = trimmedAddress;
+        this.block = new Block(addressComponents[INDEX_BLOCK]);
+        this.street = new Street(addressComponents[INDEX_STREET]);               
+        this.unit = new Unit(addressComponents[INDEX_UNIT]);
+        this.postalCode = new  PostalCode(addressComponents[INDEX_POSTALCODE]);
+                
+        this.isPrivate = isPrivate;
+        //if (!isValidAddress(trimmedAddress)) {
+        //    throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        //}
     }
 
     /**
@@ -38,19 +54,19 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.getBlock() + ", " + street.getStreet() + ", " + unit.getUnit() + ", " + postalCode.getPostalCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return this.toString().hashCode();
     }
 
     public boolean isPrivate() {
